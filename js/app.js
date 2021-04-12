b3d502fb77ca9546f7bf992a426aa0b537834367
lanaalsoufi <lanaalsoufi@outlook.com>
2021-04-12 22:42:41 +0300

    About Me

---
+++ js/app.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let userInput = prompt('Do you want to know about me?').toLowerCase();
+if(userInput == 'yes'){
+    alert('welcome');
+
+}else{
+    alert('ok');
+}
+console.log(userInput);
+ 
+
+let userInput = prompt('Is my major is English literature?');
+if(userInput == 'yes'){
+    alert('your answer is correct');
+
+}else{
+    alert('your answer is wrong');
+}
+
+
+
+let userInput = prompt('Do I study at Al-Isra university?');
+if(userInput == 'yes'){
+    alert('your answer is correct');
+
+}else{
+    alert('your answer is wrong');
+}
+
+
+let userInput = prompt('Do I study at ASAC?');
+if(userInput == 'yes'){
+    alert('your answer is correct');
+
+}else{
+    alert('your answer is wrong');
+}
+  
+
+let userInput = prompt('Do I love the winter season');
+if(userInput == 'yes'){
+    alert('your answer is wrong');
+
+}else{
+    alert('your answer is correct');
+}
+
+
+
+
